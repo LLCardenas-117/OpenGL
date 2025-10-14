8bdb03843d47c05915d6d4c6b1d0ac1e3dfcb980
LLCardenas-117 <lcardenas@student.neumont.edu>
2025-10-14 10:39:46 -0600

    Followed along in class

--- Build/Assets/Shaders/basic.vert
+++ Build/Assets/Shaders/basic.vert
@@ -3,16 +3,18 @@
 // a_### = attributes/inputs
 layout (location = 0) in vec3 a_position;
 layout (location = 1) in vec3 a_color;
-//layout (location = 2) in vec2 a_uv;
+layout (location = 2) in vec2 a_texcoord;
 
 // v_### = varyings (vertex -> fragment)
 out vec3 v_color;
+out vec2 v_texcoord;
 
 // u_### = uniform
 uniform float u_time;
 
 void main() {
 	v_color = a_color;
+	v_texcoord = a_texcoord;
 	//gl_Position = vec4(a_position + tan(u_time), 1.0);
 	float frequency = 3.0;
 	float amplitude = 0.3;
--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -15,9 +15,56 @@ int main(int argc, char* argv[]) {
     // OPEN GL
     std::vector<neu::vec3> points = { {-0.5f, -0.5f, 0}, {0.5f, 0.5f, 0}, {0.5f, -0.5f, 0}, {-0.5f, 0.5f, 0} };
     std::vector<neu::vec3> colors = { {1.0f, 0, 0 }, {0, 1, 0}, {0, 0, 1}, {1, 1, 1} };
+    std::vector<neu::vec2> texcoords{ { 0, 0 }, { 0.5f, 1.0f }, { 1, 1 } };
 
-    GLuint vbo[2];
-    glGenBuffers(2, vbo);
+    struct Vertex {
+        neu::vec3 position;
+        neu::vec3 color;
+        neu::vec2 texcoord;
+    };
+
+    std::vector<Vertex> vertices{
+        { {-0.5f, -0.5f, 0}, {1, 0, 0 }, { 0, 0 } },
+        { {-0.5f, 0.5f, 0}, {0, 1, 0 }, { 0, 1 } },
+        { {0.5f, 0.5f, 0}, {0, 0, 1 }, { 1, 1 } },
+        { {0.5f, -0.5f, 0}, {1, 1, 1 }, { 1, 0 } }
+    };
+
+    std::vector<GLuint> indices{ 0, 1, 2, 2, 3, 0 };
+
+    // Vertex Buffer
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+
+    // Index Buffer
+    GLuint ibo;
+    glGenBuffers(1, &ibo);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * indices.size(), indices.data(), GL_STATIC_DRAW);
+
+    // Vertex Array
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, position));
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, color));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void *)offsetof(Vertex, texcoord));
+
+    /*
+    GLuint vbo[3];
+    glGenBuffers(3, vbo);
 
     // Vertex Buffer (position)
     glBindBuffer(GL_ARRAY_BUFFER, vbo[0]);
@@ -26,6 +73,10 @@ int main(int argc, char* argv[]) {
     // Vertex Buffer (color)
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
     glBufferData(GL_ARRAY_BUFFER, sizeof(neu::vec3) * colors.size(), colors.data(), GL_STATIC_DRAW);
+
+    // Vertex Buffer (texcoord)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(neu::vec2) * texcoords.size(), texcoords.data(), GL_STATIC_DRAW);
 
     // Vertex Array
     GLuint vao;
@@ -41,6 +92,12 @@ int main(int argc, char* argv[]) {
     glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, vbo[1]);
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    // Texcoord
+    glEnableVertexAttribArray(2);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo[2]);
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 0, NULL);
+    */
 
     // Vertex Shader
     std::string vs_source;
@@ -100,9 +157,16 @@ int main(int argc, char* argv[]) {
     }
     glUseProgram(shaderProgram);
 
+    // Texture
+    neu::res_t<neu::Texture> texture = neu::Resources().Get<neu::Texture>("Textures/beast.png");
+
     // Uniform
     GLint uniform = glGetUniformLocation(shaderProgram, "u_time");
-    ASSERT_MSG(uniform != -1, "Could not find uniform u_time.");
+    //ASSERT_MSG(uniform != -1, "Could not find uniform u_time.");
+
+    GLint tex_uniform = glGetUniformLocation(shaderProgram, "u_texture");
+    glUniform1i(tex_uniform, 0);
+    //ASSERT_MSG(tex_uniform != 1, "Could not find uniform u_texture.");
 
     // MAIN LOOP
     while (!quit) {
@@ -121,13 +185,11 @@ int main(int argc, char* argv[]) {
 
         // draw
         neu::vec3 color{ 0, 0, 0 };
-        neu::GetEngine().GetRenderer().SetColor(color.r, color.g, color.b);
         neu::GetEngine().GetRenderer().Clear();
 
         glBindVertexArray(vao);
-        glDrawArrays(GL_TRIANGLES, 0, (GLsizei)points.size());
-
-        //glEnd();
+        //glDrawArrays(GL_TRIANGLES, 0, (GLsizei)points.size());
+        glDrawElements(GL_TRIANGLES, (GLsizei)indices.size(), GL_UNSIGNED_INT, 0);
 
         neu::GetEngine().GetRenderer().Present();
     }
